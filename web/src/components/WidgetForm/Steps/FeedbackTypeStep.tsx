0e142fe5b51a36391ae7885f72dc5e11d31b4162
gabrielmeloc22 <gabrielpresb@gmail.com>
2022-05-16 18:57:19 -0300

    first commit

---
+++ web/src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
@@ -0,0 +1,36 @@
+import { feedbackTypes } from "..";
+import { FeedbackType } from "..";
+
+import { CloseBtn } from "../../CloseBtn";
+
+
+interface FeedbackTypeStepProps {
+  onFeedbackTypeChanged: (type: FeedbackType) => void;
+}
+
+export function FeedbackTypeStep({ onFeedbackTypeChanged }: FeedbackTypeStepProps) {
+  return (
+    <>
+      <header className="text-xl leading-6">
+        <span>Deixe seu feedback</span>
+        <CloseBtn />
+      </header>
+
+      <div className="flex py-8 gap-2 w-full">
+        {Object.entries(feedbackTypes).map(([key, value]) => {
+          return (
+            <button
+              key={key}
+              className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+              onClick={() => onFeedbackTypeChanged(key as FeedbackType)}
+              type="button"
+            >
+              <img src={value.image.source} alt={value.image.alt} />
+              <span>{value.title}</span>
+            </button>
+          );
+        })}
+      </div>
+    </>
+  )
+}
